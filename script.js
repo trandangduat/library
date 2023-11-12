21f824b656a63d6180ffa6fa7d17a2333f673fd5
Tran Dang Duat <23020023@vnu.edu.vn>
2023-11-12 15:16:57 +0700

    i forgot to commit ;(, it's just the HTML structure + some css anyway

---
+++ script.js
@@ -0,0 +1,48 @@
+const myLibrary = [];
+
+function Book (name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+let bookContainer = document.getElementById("books-container");
+let newBookButton = document.getElementById("new-book-button");
+let newBookBoard = document.getElementById("new-book-board");
+let newBookSubmitButton = document.getElementById("new-book-submit");
+
+function addBookToLibrary (name, author, pages, read) {
+    let newBook = new Book(name, author, pages, read);
+    myLibrary.push(newBook);
+    let latestBookAdded = document.querySelector("#books-container .book:nth-child(2)");
+    console.log(latestBookAdded);
+    latestBookAdded.insertAdjacentHTML("beforebegin",
+    `
+        <div class = "book">
+            <div class = "title">${name}</div>
+            <div class = "author">${author}</div>
+        </div>
+    `);
+    return newBook;
+}
+
+newBookButton.addEventListener("click", (e) => {
+    newBookBoard.toggleAttribute("style");
+});
+
+newBookSubmitButton.addEventListener("click", (e) => {
+    let name = document.getElementById("new-book-title").value;
+    let author = document.getElementById("new-book-author").value;
+    let pages = document.getElementById("new-book-pages").value;
+    if (!name || !author || !pages) {
+        return;
+    }
+    e.preventDefault();
+    addBookToLibrary(name, author, pages, false);
+    document.getElementById("new-book-form").reset();
+});
+
+
+
+// console.log(myLibrary);
